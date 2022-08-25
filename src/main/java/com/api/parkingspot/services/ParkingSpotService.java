85501a200499ae40590c2eae112b1e041919543f
Gerson Junior <gersonjunior2@outlook.com>
2022-08-25 17:59:47 -0300

    criação de estrutura de pastas do projeto

---
+++ src/main/java/com/api/parkingspot/services/ParkingSpotService.java
@@ -0,0 +1,14 @@
+package com.api.parkingspot.services;
+
+import org.springframework.stereotype.Service;
+import com.api.parkingspot.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+	
+	final ParkingSpotRepository parkingSpotRepository;
+	
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+}
